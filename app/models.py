012059dbe5ff36eecd32197657492ae90fe257d8
kush <kushagrasp8793@gmail.com>
2023-03-10 17:12:02 +0530

    updated

---
+++ app/models.py
@@ -0,0 +1,106 @@
+from app import db
+from flask_login import UserMixin
+
+
+
+class User(UserMixin, db.Model):
+
+    id = db.Column(db.Integer,     primary_key=True)
+    user = db.Column(db.String(64),  unique = True)
+    email = db.Column(db.String(120), unique = True)
+    password = db.Column(db.String(500))
+# admin fild
+    admin = db.Column(db.Boolean, default=False)
+    def __init__(self, user, email, password ,admin):
+        self.user       = user
+        self.password   = password
+        self.email      = email
+        self.admin      = admin
+
+    def __repr__(self):
+        return str(self.id) + ' - ' + str(self.user)
+
+    def save(self):
+        # inject self into db session    
+        db.session.add ( self )
+        # commit change and save the object
+        db.session.commit()
+        return self 
+class Venue(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64),  unique = True)
+    place = db.Column(db.String(120), unique = True)
+    capacity = db.Column(db.Integer)
+    shows = db.relationship('Show', backref='venue')
+    userid = db.Column(db.Integer, db.ForeignKey('user.id'))
+    def __init__(self, name, place, capacity , userid):
+        self.name       = name
+        self.place      = place
+        self.capacity   = capacity
+        self.userid     = userid
+
+    def __repr__(self):
+        return str(self.id) + ' - ' + str(self.name)
+
+    def save(self):
+        # inject self into db session    
+        db.session.add ( self )
+        # commit change and save the object
+        db.session.commit()
+        return self
+
+
+class   Show(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64),  unique = True)
+    rating = db.Column(db.Integer)
+    tags = db.Column(db.String(500))
+    ticketPrice = db.Column(db.Integer)
+    startTime = db.Column(db.DateTime)
+    endTime = db.Column(db.DateTime)
+    date = db.Column(db.DateTime)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'))
+    def __init__(self, name, rating, tags, ticketPrice, venue_id , date, startTime, endTime):
+        self.name       = name
+        self.rating     = rating
+        self.tags       = tags
+        self.ticketPrice = ticketPrice
+        self.venue_id   = venue_id
+        self.date       = date
+        self.startTime  = startTime
+        self.endTime    = endTime
+
+
+    def __repr__(self):
+        return str(self.id) + ' - ' + str(self.name)
+
+    def save(self):
+        # inject self into db session    
+        db.session.add ( self )
+        # commit change and save the object
+        db.session.commit()
+        return self
+
+class   Ticket(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    show_id = db.Column(db.Integer, db.ForeignKey('show.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    show = db.relationship('Show', backref='tickets')
+    user = db.relationship('User', backref='tickets')
+
+    def __init__(self, show_id, user_id):
+        self.show_id    = show_id
+        self.user_id    = user_id
+
+    def __repr__(self):
+        return str(self.id) + ' - ' + str(self.show_id) + ' - ' + str(self.user_id)
+
+    def save(self):
+        # inject self into db session    
+        db.session.add ( self )
+        # commit change and save the object
+        db.session.commit()
+        return self
+
+
+
---
+++ app/views.py
@@ -0,0 +1,136 @@
+# Python modules
+import os, logging 
+
+# Flask modules
+from flask  import render_template, request, url_for, redirect, send_from_directory
+from flask_login  import login_user, logout_user, current_user, login_required
+from werkzeug.exceptions import HTTPException, NotFound, abort
+from jinja2  import TemplateNotFound
+
+# App modules
+from app import app, lm, db, bc
+from app.models import User , Venue , Show
+from app.forms import LoginForm, RegisterForm
+
+# provide login manager with load_user callback
+@lm.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Logout user
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+# Register a new user
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # declare the Registration Form
+    form = RegisterForm(request.form)
+    msg     = None
+    success = False
+    if request.method == 'GET': 
+        return render_template( 'accounts/register.html', form=form, msg=msg )
+    # check if both http method is POST and form is valid on submit
+# get  data from html form
+    try:
+        if (request.form['admin']):
+            admin = True
+        else:
+            admin = False
+    except:
+        admin = False
+
+    if form.validate_on_submit():
+        # assign form data to variables
+        username = request.form.get('username', '', type=str)
+        password = request.form.get('password', '', type=str) 
+        email    = request.form.get('email'   , '', type=str) 
+        # filter User out of database through username
+        user = User.query.filter_by(user=username).first()
+        # filter User out of database through username
+        user_by_email = User.query.filter_by(email=email).first()
+        if user or user_by_email:
+            msg = 'Error: User exists!'
+        else:         
+            pw_hash = password
+            user = User(username, email, pw_hash , admin)
+            user.save()
+            msg = 'User created, please <a href="' + url_for('login') + '">login</a>'     
+            success = True
+        return render_template( 'accounts/register.html', form=form, msg=msg, success=success)
+    else:
+        msg = 'Input error'     
+    return render_template( 'accounts/register.html', form=form, msg=msg, success=success )
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Declare the login form
+    form = LoginForm(request.form)
+    # Flask message injected into the page, in case of any errors
+    msg = None
+    # check if both http method is POST and form is valid on submit
+    try : 
+        if (request.form['admin']):
+            admin = True
+        else:
+            admin = False
+    except:
+        admin = False
+    if form.validate_on_submit():
+        # assign form data to variables
+        username = request.form.get('username', '', type=str)
+        password = request.form.get('password', '', type=str) 
+        # filter User out of database through username
+        user = User.query.filter_by(user=username).first()
+        if user:
+            if  password  == user.password:
+                login_user(user)
+                return redirect(url_for('index'))
+            else:
+                msg = "Wrong password. Please try again."
+        else:
+            msg = "No user registerd with this usename " 
+    return render_template( 'accounts/login.html', form=form, msg=msg )
+
+@app.route('/')
+def index():
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    else:
+        user = User.query.filter_by(user=current_user.user).first()
+        if user.admin == True :
+           view = Venue.query.filter_by(userid = user.id).all()
+
+           return render_template('admin.html' , view = view)
+        else:
+            return  render_template('index.html')
+        
+@app.route('/createvenue' , methods=['GET', 'POST'])
+def createvenue():
+    if not current_user.is_authenticated : 
+        return redirect(url_for('login'))
+    else:
+        user = User.query.filter_by(user=current_user.user).first()
+        if user.admin == True :
+            if request.method == 'GET':
+                return render_template('admin/CreateVenue.html')
+            else:
+                name = request.form.get('name', '', type=str)
+                address = request.form.get('address', '', type=str)
+                capacity = request.form.get('Capacity', '', type=int)
+                venue = Venue(name, address, capacity  , user.id)
+                venue.save()
+                return redirect(url_for('login'))
+        else:
+            return  render_template('index.html')
+@app.route('/createshow' , methods=['GET', 'POST'])
+def create_show():
+    if not current_user.is_authenticated : 
+        return redirect(url_for('login'))
+    else:
+        user = User.query.filter_by(user=current_user.user).first()
+        if user.admin == True :
+            return render_template('admin/createshow.html')
+        else:
+            return  render_template('index.html')
